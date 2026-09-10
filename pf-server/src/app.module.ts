404094f48e182f83352e867d38b71bf95aafa87d
João Macaoli <devjoaom@gmail.com>
2026-09-10 08:36:21 -0300

    feat(checkin): create checkin resource

--- pf-server/src/app.module.ts
+++ pf-server/src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
+import { CheckinModule } from './checkin/checkin.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
-    ObserveModule.forRoot({
-      appKey: 'YOUR_APP_KEY',
-      appSecret: 'YOUR_APP_SECRET',
-      serviceId: 'pf-server',
-    }),
+    // ObserveModule.forRoot({
+    //   appKey: 'YOUR_APP_KEY',
+    //   appSecret: 'YOUR_APP_SECRET',
+    //   serviceId: 'pf-server',
+    // }),
+    CheckinModule,
   ],
   controllers: [],
   providers: [],
--- pf-server/src/main.ts
+++ pf-server/src/main.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
