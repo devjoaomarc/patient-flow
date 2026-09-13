2dd57292c289014c45c17362b7b62bb60d93c66b
João Macaoli <devjoaom@gmail.com>
2026-09-13 17:28:34 -0300

    chore: configure dotenv validationpipe and typeorm

--- pf-server/src/app.module.ts
+++ pf-server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { CheckinModule } from './checkin/checkin.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -11,6 +13,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
     //   appSecret: 'YOUR_APP_SECRET',
     //   serviceId: 'pf-server',
     // }),
+    TypeOrmModule.forRoot(databaseConfig),
     CheckinModule,
   ],
   controllers: [],
--- pf-server/src/main.ts
+++ pf-server/src/main.ts
@@ -1,5 +1,8 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +12,8 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
